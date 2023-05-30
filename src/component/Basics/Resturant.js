a635cbcde0d90383f37306115a3d588b20240248
Nitish chaurasiya <nitishchaurasiyac555@gmail.com>
2023-05-31 02:29:34 +0530

    search updated and restaurant app completed

--- src/component/Basics/Resturant.js
+++ src/component/Basics/Resturant.js
@@ -1,29 +1,39 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Menu from './menuApi'
 import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList = [...new Set(
+    Menu.map((curElem) => {
+        return curElem.category
+    })
+),"All",
+];
+console.log(uniqueList);
+
 const Resturant = () => {
     const [menuData, setMenuData] = useState(Menu)
+    // eslint-disable-next-line
+    const [menuList, setMenuList] = useState(uniqueList)
 
-    const filterItem = (category)=>{
-        const updatedList = Menu.filter((curElem)=>{
+    const filterItem = (category) => {
+
+        if(category === "All"){
+            setMenuData(Menu)
+            return
+        }
+
+        const updatedList = Menu.filter((curElem) => {
             return curElem.category === category;
         })
         setMenuData(updatedList)
     }
-  return (
-    <>
-    <nav className="navbar">
-        <div className="btn-group">
-            <button className="btn-group__item" onClick={()=>filterItem("breakfast")} >Breakfast</button>
-            <button className="btn-group__item" onClick={()=>filterItem("lunch")}>Lunch</button>
-            <button className="btn-group__item" onClick={()=>filterItem("evening")}>Evening</button>
-            <button className="btn-group__item" onClick={()=>filterItem("dinner")}>Dinner</button>
-            <button className="btn-group__item" onClick={()=>setMenuData(Menu)}>All</button>
-        </div>
-    </nav>
-    <MenuCard menuData={menuData}/>
-    </>
-  )
+    return (
+        <>
+            <Navbar filterItem ={filterItem} menuList={menuList}/>
+            <MenuCard menuData={menuData} />
+        </>
+    )
 }
 
 export default Resturant
